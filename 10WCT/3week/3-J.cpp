e4d39ceef56515a861b4d98c92f240fd045e30c8
LAPTOP-SIHWAN\psh42 <psh4204@naver.com>
2024-09-03 23:29:01 +0900

    14497

--- 10WCT/3week/3-J.cpp
+++ 10WCT/3week/3-J.cpp
@@ -19,6 +19,17 @@ Try1
 - 최대 300x300 짜리 면 단순하게 짜도 상관없을듯
 - 주난의 주위의 0친구들을 어디 저장해둬야함
 - 범인위치까지 닿을때까지 단순하게 BFS 해보자
+- 이건 한 루프마다 플루드필을 해야하는 문제다.
+- 이미 터진곳은 4처리하자.
+
+        // TEST CODE
+        cout << "\n" << _cnts << "\n";
+        for(int y = 0; y < _Y; y++)
+        {
+            for(int x = 0; x < _X; x++)
+                cout << _room[y][x];
+            cout << "\n";
+        }
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -27,12 +38,15 @@ int _dy[4] = {-1, 0, 1, 0};
 int _dx[4] = {0, 1, 0, -1};
 vector<vector<int>> _room;
 pair<int,int> _bg;  // badguy
-pair<int,int> _jn;  // 주난
-vector<pair<int,int>> _zeros;   // 주난 근처 0값들
+pair<int,int> _joo;  // 주난
+queue<pair<int,int>> _q;    // 주난을 중심으로 터져나간다.
+
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> _Y >> _X;
+    cin >> _Y >> _X >> _joo.first >> _joo.second >>  _bg.first >> _bg.second;
+    _joo.first--;_joo.second--;_bg.first--;_bg.second--;
+
     _room.resize(_Y, vector<int>(_X));
     for(int y = 0; y < _Y; y++)
     {
@@ -46,17 +60,55 @@ int main()
                 _room[y][x] = 0;
             else if(in_ch == '1')
                 _room[y][x] = 1;
-            else if(in_ch == '#')
-            {
-                _room[y][x] = 2;
-                _bg = {y,x};
-            }
             else if(in_ch == '*')
             {
+                _room[y][x] = 2;
+            }
+            else if(in_ch == '#')
+            {
                 _room[y][x] = 3;
-                _jn = {y,x};
             }
         }
     }
+    
+    _q.push(_joo);
+    _room[_joo.first][_joo.second] = 4;
+    do
+    {
+        vector<pair<int,int>> boom_list;
+        do
+        {
+            pair<int,int> boom = _q.front(); _q.pop();
+            for(int di = 0; di < 4; di++)
+            {
+                int ny = boom.first + _dy[di];
+                int nx = boom.second + _dx[di];
+                if(ny < 0 || nx < 0 || ny >= _Y || nx >= _X) continue;
+                if(_room[ny][nx] == 1 || _room[ny][nx] == 3)
+                {
+                    boom_list.push_back({ny,nx});
+                    _room[ny][nx] = 4;
+                }
+                
+                else if(_room[ny][nx] == 0)
+                {
+                    _q.push({ny,nx});
+                    _room[ny][nx] = 4;
+                }
+            }
+
+        }while(!_q.empty());
+
+        // 붐리스트 0으로 만들며 큐에 삽입
+        for(auto boom : boom_list)
+        {
+            _q.push(boom);
+        }
+        
+        _cnts++;
+    }while(_room[_bg.first][_bg.second] != 4);
+
+    cout << _cnts;
+
     return 0;
 }
