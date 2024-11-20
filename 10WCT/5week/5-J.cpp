596b74ed330ebfbf65722bf6306453495d67b73f
박시환 <52143252+psh4204@users.noreply.github.com>
2024-11-20 14:20:31 +0900

    (resolve) 1700
    운영체제에서 페이징 알고리즘 중 Optimal 알고리즘 적용.
    
    Optimal 알고리즘: 태스크 내에서 각자 바뀌는 시점에서 가장 나중에 사용될 페이지를 먼저 교체하는것. 
    
    - 우선순위큐로 풀려고 했으나, 전혀다른 알고리즘(Opt)으로 풀어야했음
    - 페이지 스와핑 최소화 -> Optimal 과 우선순위 큐 둘중 하나 사용.

--- 10WCT/5week/5-J.cpp
+++ 10WCT/5week/5-J.cpp
@@ -15,20 +15,21 @@ https://www.acmicpc.net/problem/1700
     - 멀티탭을 우선순위 큐로 두자 (사용적은 순으로 top에 두기)
 - 그렇게 pop되는 횟수 카운트 하면 풀리는 문제
 
-힌트
-- 우선순위큐 아님
-- Optimal 알고리즘을 사용해야함. (가장 먼 미래에 사용되는 애를 바꾸면 안됨)
+힌트듣고 풀이
+- 우선순위큐 아님 (자주사용하는 것으로 우선순위 구하면 틀림)
+- Optimal 알고리즘을 사용해야함. 
+    - 페이징 최소화 알고리즘 중 하나
+    - 가장 나중에 사용할 페이지를 교체하는 알고리즘. (https://rob-coding.tistory.com/37)
     - 와;;
-    - TODO: 새로짜자!!! (새 알고리즘 적용해야하니까.. ㅠㅠ)
+- 유의사항: 다음에 사용안할 페이지는 곧장 교체해주면 됨.
 */
 #include <bits/stdc++.h>
-#define MAX_NUM 101
-#define IS_TEST true
+#define MAX_NUM     101
+#define IS_TEST     false
 using namespace std;
 int _N, _K, _ret_cnt;
-vector<int> _devices(MAX_NUM);
-vector<bool> _is_push_lst(false);
-queue<int> _history;
+vector<bool> _is_plugged_list(MAX_NUM, false);
+vector<int> _plug_list;
 vector<int> _multitab;
 
 int main()
@@ -38,42 +39,64 @@ int main()
     {
         int device;
         cin >> device;
-        _history.push(device);
-        _devices[device]+=1;
+        _plug_list.push_back(device);
     }
     for(int k_cnt = 0; k_cnt < _K; k_cnt++)
     {
-        if(IS_TEST) cout << "current_dev: "<< _history.front() << " // ";
-        // 1. 멀티탭 구멍이 남으면,
+        int cur_device = _plug_list[k_cnt];
+        if(IS_TEST) cout << "current_dev: "<< cur_device << " // ";
+        // 1. 멀티탭 구멍이 남았다면, 넣고 본다.
         if(_multitab.size() < _N)
         {
-            // 1.1. 멀티탭이 비었거나, 기기를 안꼽았다면 꼽는다
-            if(_multitab.empty() || !_is_push_lst[_history.front()])
+            if(_is_plugged_list[cur_device])
             {
-                if(IS_TEST) cout << "INSERT\n";
-                _multitab.push({_devices[_history.front()], _history.front()});
-                _is_push_lst[_history.front()] = true;
+                if(IS_TEST) cout << "ALREADY\n";
+                continue;
             }
-            else
-                if(IS_TEST) cout << "PAUSE\n";
-            _history.pop();
+            if(IS_TEST) cout << "PUSH\n";
+            _multitab.push_back(cur_device);
+            _is_plugged_list[cur_device] = true;
             continue;
         }
-        // 2. 멀티탭 구멍이 꽉찼는데, 현재 기기가 멀티탭에 꼽혀있다면 그대로 둔다.
-        else if(_is_push_lst[_history.front()])
+
+        // 2. 멀티탭 구멍이 꽉 차있다면, 
+        // 2.1. 멀티탭에 같은 기기가 꽂힌지 확인
+        if(_is_plugged_list[cur_device])
         {
-            if(IS_TEST) cout << "PAUSE\n";
-            _history.pop();
+            if(IS_TEST) cout << "ALREADY\n";
             continue;
+        } 
+
+        // 2.2. 플러그를 뽑아야하면, Optimal 알고리즘 사용(가장 나중에 사용할 플러그부터 교체)
+        int last_plug_idx = 0;  // 가장 마지막에 바뀔 플러그의 순서.
+        int pull_idx = 0;           // 멀티탭에서 뽑아야할 인덱스
+        for(int mt_idx = 0; mt_idx < _N; mt_idx++)
+        {
+            int sub_last_idx = MAX_NUM;
+            for(int plug_idx = k_cnt+1; plug_idx<_K; plug_idx++)
+            {
+                if(_plug_list[plug_idx] == _multitab[mt_idx])
+                {
+                    sub_last_idx = plug_idx;
+                    break;
+                }
+            }
+            // 현재 멀티탭의 기기가 두번다시 안꽂는 거라면 바로 교체
+            if(sub_last_idx == MAX_NUM)
+            {
+                pull_idx = mt_idx;
+                break;
+            }
+            // 아니라면 마지막 순서 업데이트
+            if(last_plug_idx > sub_last_idx) continue;
+            last_plug_idx = sub_last_idx;
+            pull_idx = mt_idx;
         }
-        // 3. 새로운 기기를 꼽기위해, 멀티탭에서 다른 기기를 뽑고, 새롭게 꼽는다.
-        if(IS_TEST) cout << "POP: "<< _multitab.top().second << " // INSERT: " << _history.front() << "\n";
-        _is_push_lst[_multitab.top().second] = false;
-        _multitab.pop();
-        _multitab.push({_devices[_history.front()], _history.front()});
-        _is_push_lst[_history.front()] = true;
-        _history.pop();
-        _ret_cnt++;
+        if(IS_TEST) cout << " PULL: " << _multitab[pull_idx] << " PUSH: " << cur_device << "\n";
+        _is_plugged_list[_multitab[pull_idx]] = false;
+        _multitab[pull_idx] = cur_device;
+        _is_plugged_list[cur_device] = true;
+        _ret_cnt+=1;
     }
     cout << _ret_cnt;
     return 0;
