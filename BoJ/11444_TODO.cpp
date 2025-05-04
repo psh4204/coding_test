e7b327dfb56dad434053714d6ba5fe4d505419b0
LAPTOP-SIHWAN\psh42 <psh4204@naver.com>
2025-05-04 14:31:07 +0900

    boj 11444
    TODO: Solve it

--- BoJ/11444_TODO.cpp
+++ BoJ/11444_TODO.cpp
@@ -13,16 +13,34 @@ N번째 피보나치 수를 구하라 (0~1,000,000,000,000,000,000)
 - 1,000,000,007로 모듈러 한 N번째 피보나치 수를 구한다
 
 풀이
-- ㅋㅋㅋ 못풀겠다! 답안보고 한다 그냥.. 
-- 
+- ㅋㅋㅋ 못풀겠다! 답안보고 한다 그냥.. 그냥 수학공식이잖아.. ㅠㅠ (문과는 Fail)
+
+[Note]
+- 다음의 행렬의 거듭제곱으로 피보나치 수를 풀 수 있음
+->  M^1 =   ((1 1))
+            ((1 0))
+- 여기서 좌측하단의 값이 구하고자 하는 값
+
+
+TODO: 그냥 나중에 풀자~ ㅋㅋㅋ 하아
 */
+
 #include <bits/stdc++.h>
 using namespace std;
 #define MODL 1000000007
 typedef long long ll;
 
+ll g_N;
+vector<vector<int,int>> g_nums = {{1,1},{1,0}};
+
+ll fibo(int n)
+{
+
+}
+
 int main()
 {
+    cin >> g_N;
 
     return 0;
 }
